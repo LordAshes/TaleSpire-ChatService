90b4fadb8617d33e8d009702c473d0f647c65c6c
Lord Ashes <webtable@gmail.com>
2021-10-11 15:51:39 -0400

    Version 1.0.1
    Initial release

---
+++ TaleSpireChatServicePlugin/Patches/Patch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using BepInEx;
+using GameChat.UI;
+using HarmonyLib;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace LordAshes
+{
+    public partial class ChatServicePlugin : BaseUnityPlugin
+    {
+        [HarmonyPatch(typeof(UIChatMessageManager), "AddChatMessage")]
+        public static class PatchAddChatMessage
+        {
+            public static bool Prefix(string creatureName, Texture2D icon, ref string chatMessage, UIChatMessageManager.IChatFocusable focus = null)
+            {
+                foreach(KeyValuePair<string,Func<string,string, ChatSource, string>> handler in ChatServicePlugin.handlers)
+                {
+                    if (chatMessage.StartsWith(handler.Key))
+                    {
+                        chatMessage = handler.Value(chatMessage, creatureName, FindSource(creatureName));
+                        if (chatMessage == null) { return false; }
+                    }
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(UIChatMessageManager), "AddEventMessage")]
+        public static class PatchAddEventMessage
+        {
+            public static bool Prefix(string title, ref string message, UIChatMessageManager.IChatFocusable focus = null)
+            {
+                foreach (KeyValuePair<string, Func<string, string, ChatSource, string>> handler in ChatServicePlugin.handlers)
+                {
+                    if (message.StartsWith(handler.Key))
+                    {
+                        message = handler.Value(message, title, FindSource(title));
+                        if (message == null) { return false; }
+                    }
+                }
+                return true;
+            }
+        }
+
+        private static ChatSource FindSource(string name)
+        {
+            foreach(CreatureBoardAsset asset in CreaturePresenter.AllCreatureAssets)
+            {
+                if(asset.Creature.Name.StartsWith(name))
+                {
+                    return ChatSource.creature;
+                }
+            }
+            foreach (KeyValuePair<PlayerGuid, PlayerInfo> player in CampaignSessionManager.PlayersInfo)
+            {
+                if(CampaignSessionManager.GetPlayerName(player.Key)==name)
+                {
+                    if (player.Value.Rights.CanGm) { return ChatSource.gm; } else { return ChatSource.player; }
+                }
+            }
+            return ChatSource.anonymous;
+        }
+    }
+}
---
+++ TaleSpireChatServicePlugin/Program.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Newtonsoft.Json;
+using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Collections;
+
+namespace LordAshes
+{
+	[BepInPlugin(Guid, Name, Version)]
+	public partial class ChatServicePlugin : BaseUnityPlugin
+	{
+		// Plugin info
+		public const string Name = "Chat Service Plug-In";
+		public const string Guid = "org.lordashes.plugins.chatservice";
+		public const string Version = "1.0.1.0";
+
+        public enum ChatSource
+        {
+            gm = 0,
+            player = 1,
+            creature = 2,
+            anonymous = 999
+        }
+
+        public static Dictionary<string, Func<string, string, ChatSource, string>> handlers = new Dictionary<string, Func<string,string, ChatSource, string>>();
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+		{
+			UnityEngine.Debug.Log("Chat Service Plugin: Active.");
+
+            UnityEngine.Debug.Log("Chat Service Plugin: Patching.");
+            var harmony = new Harmony(Guid);
+			harmony.PatchAll();
+        }
+
+        void Update()
+        {
+        }
+    }
+}
